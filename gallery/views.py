96f8dcedf94398adde9d131a49500c39d9c667ec
MungaiKeren <wambukeren@gmail.com>
2019-10-04 13:05:03 +0300

    This commit creates template files... navbar,base, modified index and used bootstrap

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    title = 'MyGallery'
+    today = dt.datetime.today()
+    return render(request, 'index.html', {"title": title, "today": today})
