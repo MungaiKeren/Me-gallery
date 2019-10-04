dc8d3c9f2785395c9762149bd7d55a24416f197f
MungaiKeren <wambukeren@gmail.com>
2019-10-04 17:29:37 +0300

    This commit recreates migrations, and changes location and category postions

--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to='pic_folder/')
-    image_name = models.CharField(max_length=30)
-    image_description = models.TextField()
-
-    def __str__(self):
-        return self.image_name
-
-
 class Location(models.Model):
     location = models.CharField(max_length=20)
 
@@ -23,3 +14,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+
+class Image(models.Model):
+    """
+    image class model that gets our image
+    """
+    image = models.ImageField(upload_to='pic_folder/')
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+
+    def __str__(self):
+        return self.image_name
