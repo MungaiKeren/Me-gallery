c83e63272afcd0b00cd31fb6521646a552c0162c
MungaiKeren <wambukeren@gmail.com>
2019-10-04 16:11:54 +0300

    This commit creates image,location and category model classes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Me-gallery
 A personal photo gallery!
+LICENSE
+[MIT](https://github.com/MungaiKeren/Me-gallery/blob/master/LICENSE)
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='pic_folder/')
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+
+    def __str__(self):
+        return self.image_name
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.location
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category
