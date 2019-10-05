88cbfd5dd2b99c2f5f7cc510436a54f21c39d04c
MungaiKeren <wambukeren@gmail.com>
2019-10-05 17:13:37 +0300

    This commit creates location and category views

--- gallery/views.py
+++ gallery/views.py
@@ -15,3 +15,19 @@ def index(request):
         "all_images": all_images
     }
     return render(request, 'index.html', param)
+
+
+def location(request):
+    locate = Location.get_location()
+    param = {
+        "locate": locate
+    }
+    return render(request, 'locations.html', param)
+
+
+def category(request):
+    categorize = Category.get_all_categories()
+    param = {
+        "categorize": categorize
+    }
+    return render(request, 'category.html', param)
